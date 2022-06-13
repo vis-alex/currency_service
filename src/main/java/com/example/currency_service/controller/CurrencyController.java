4585b4fc85b2d5e36b3f117e80ef7d6a92497077
Alex-Vis <usenkoshurik@mail.ru>
2022-06-13 22:57:48 +0300

    Add retrieve yesterday rates method

--- src/main/java/com/example/currency_service/controller/CurrencyController.java
+++ src/main/java/com/example/currency_service/controller/CurrencyController.java
@@ -5,6 +5,12 @@ import com.example.currency_service.beans.CurrencyRatesBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.text.DateFormat;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 public class CurrencyController {
@@ -14,10 +20,21 @@ public class CurrencyController {
         this.proxy = proxy;
     }
 
-    @GetMapping("/get_gif")
-    public String getGif(Model model) {
-        CurrencyRatesBean currencyRates = proxy.retrieveCurrencyRates();
-        System.out.println(currencyRates);
+    @GetMapping("/get_gif/{code}")
+    public String getGif(@PathVariable String code, Model model) {
+        CurrencyRatesBean currentCurrencyRates = proxy.retrieveCurrentCurrencyRates();
+
+        Date yesterdayDate = new Date(System.currentTimeMillis()-24*60*60*1000);
+
+        Format formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String yesterdayDateString = formatter.format(yesterdayDate);
+
+        CurrencyRatesBean yesterdayCurrencyRates = proxy.retrieveYesterdayCurrencyRates(yesterdayDateString);
+        System.out.println("CurrentRates" + currentCurrencyRates);
+        System.out.println("YesterdayRates" + yesterdayCurrencyRates);
+
+        Double currentRate = currentCurrencyRates.getRates().get(code);
+        Double yesterdayRate = yesterdayCurrencyRates.getRates().get(code);
 
 
 //        model.addAttribute("rates", currencyRates);
